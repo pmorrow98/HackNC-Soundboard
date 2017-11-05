59d0e9f3f71134654f1538179d9758586cbcf312
Jack Harrison <jackharrison@dhcp-v880-22-00930.1xwireless.unc.edu>
2017-11-04 23:00:55 -0400

    sixth commit

--- soundboard.ts
+++ soundboard.ts
@@ -55,7 +55,7 @@ function main(): void {
             addPicture("./bigshaq.jpg");
         }else if (event.keyCode === 75) {
             sound = new Audio("./goodmansnothot/just sauce.mp3");
-            addPicture("./images/Kris.jpeg");
+            addPicture("./images/bigshaq.jpg");
         }else if (event.keyCode === 76) {
             sound = new Audio("./goodmansnothot/yo.mp3");
             addPicture("./bigshaq.jpg");
